ac31b598b2b401b71fe82a09365a53c23d4aa15a
Koutawars <admikouta@gmail.com>
2021-04-19 19:54:00 -0500

    Estilo input comentarios

--- entrys/views.py
+++ entrys/views.py
@@ -20,7 +20,7 @@ class singleEntry(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comments'] = Comment.objects.filter(id=self.kwargs['pk'])
+        context['comments'] = Comment.objects.filter(entryId=self.kwargs['pk'])
         return context
     
 def commentEntry(request, entryId):
