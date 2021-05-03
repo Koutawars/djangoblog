d7c3f95660fdc8f35e48bf05459e6627efe9b4a3
Koutawars <admikouta@gmail.com>
2021-05-03 15:40:36 -0500

    Registrarse

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path("logout/", LogoutView.as_view(), name='logout'),
+    path('register/', views.Register.as_view(), name="register"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import redirect, render
 from django.views.generic import FormView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic.edit import CreateView
+
+from .forms import UserRegisterForm
 from django.contrib.auth.models import User
 
 from django.urls import reverse_lazy
@@ -7,3 +11,9 @@ from django.urls import reverse_lazy
 def index(request):
     response = redirect('login/')
     return response
+
+class Register(SuccessMessageMixin, CreateView):
+    template_name = 'accounts/register.html'
+    success_url = reverse_lazy('accounts:login')
+    form_class = UserRegisterForm
+    success_message = "La cuenta se ha creado."
